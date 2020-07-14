530e58a8ab9e6e86b6d38448d6200d704b7a22e0
HandsomeGrimm <labilabizy@163.com>
2020-07-14 17:23:40 +0800

    2020.07.14

--- src/最长回文子串/Solution.java
+++ src/最长回文子串/Solution.java
@@ -1,6 +1,8 @@
 package 最长回文子串;
 
 
+import sun.plugin2.applet.context.NoopExecutionContext;
+
 /**
  * 给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
  *
@@ -11,5 +13,50 @@ package 最长回文子串;
  */
 public class Solution {
 
+    //动态规划
+    public static String longestPalindrome(String s) {
+        int len=s.length();
+
+        if (len<2){
+            return s;
+        }
+
+        int maxLen=1;
+        int begin=0;
+
+        boolean [] [] dp=new boolean[len][len];
+        for (int i=0;i<len;i++){
+            dp[i][i]=true;
+        }
+        char[] ss=s.toCharArray();
+
+        for (int j=1;j<len;j++){
+            for (int i=0;i<j;i++){
+                if (ss[i]!=ss[j]){
+                    dp[i][j]=false;
+                }else {
+                    if (j-i<3){
+                        dp[i][j]=true;
+                    }else {
+                        dp[i][j]=dp[i+1][j-1];
+                    }
+                }
+
+                if (dp[i][j]&&j-i+1>maxLen){
+                    //长度
+                    maxLen=j-i+1;
+                    begin=i;
+                }
+            }
+        }
+        return s.substring(begin,begin+maxLen);
+    }
+
+
+    public static void main(String[] args) {
+        String s="abcbaaa";
+        System.out.println(longestPalindrome(s));
+
+    }
 
 }
